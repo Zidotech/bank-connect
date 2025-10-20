6239a352e82e6081110f8564739be404f09f64f5
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2025-10-20 11:01:37 +0100

    fix: issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
-const { Colors, Fonts } = require('./constants/theme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        ...Colors.light,
-        ...Colors.dark,
-      },
-      fontFamily: {
-        sans: Fonts.sans,
-        serif: Fonts.serif,
-        mono: Fonts.mono,
+        tint: '#0a7ea4',
       },
     },
   },
